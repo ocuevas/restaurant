f5e86e4ed28e633e4bff864ee95e9ea11ca36da0
Onasi Cuevas <onasic96@gmail.com>
2019-03-17 15:02:14 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace project21
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            string[] workers = {"Luis", "Pedro", "Chris", "Perla"};
+            Random r = new Random();
+            
+            Console.WriteLine("**********************");
+            Console.WriteLine("Welcome to La Familia Restaurante");
+            Console.WriteLine("Hello! My name is  {0}, and going to take your order today", workers[r.Next(0,5)]);
+
+        }
+
+        
+    }
+}
