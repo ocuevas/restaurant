51174ab349acfd087cb4735254f2e411df7d213c
Onasi Cuevas <onasic96@gmail.com>
2019-03-17 15:04:47 -0400

    another commit

--- Program.cs
+++ Program.cs
@@ -8,13 +8,15 @@ namespace project21
 
             string[] workers = {"Luis", "Pedro", "Chris", "Perla"};
             Random r = new Random();
-            
+
             Console.WriteLine("**********************");
             Console.WriteLine("Welcome to La Familia Restaurante");
             Console.WriteLine("Hello! My name is  {0}, and going to take your order today", workers[r.Next(0,5)]);
 
-        }
 
-        
+ // loop until you finished orden
+
+        }
+       
     }
 }
---
+++ Restaurant.cs
@@ -0,0 +1,17 @@
+class Restaurant{
+
+// Instentive variables 
+    private int cost_of_food = 0;
+
+
+
+
+//Methods
+    //Display Menu
+    public void displayMenu(){
+        
+    }
+    //Select the food 
+    //Pay
+    // Return change
+}
