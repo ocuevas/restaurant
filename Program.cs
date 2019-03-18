d6de0143787e3bcd2679006e98417ab81921dcdc
Onasi Cuevas <onasic96@gmail.com>
2019-03-18 12:30:48 -0400

    loop until the user end ordering

--- Program.cs
+++ Program.cs
@@ -11,17 +11,65 @@ namespace project21
             Random r = new Random();
             Restaurant restaurant = new Restaurant();
             Console.WriteLine("**********************");
-            Console.WriteLine("Welcome G Press!");
-            Console.WriteLine("Hello! My name is  {0}, and going to take your order today", workers[r.Next(0,4)]);
-            Console.Write("Press 1 to show the menu ");
+            Console.WriteLine("Welcome to Caribbean Taste!");
+            Console.WriteLine("Hello! My name is  {0}, and going to take your order today.", workers[r.Next(0,4)]);
+            Console.Write("Please press 1 to show menu: ");
             showMenu = int.Parse(Console.ReadLine());
+            
+           int n = 0;
+           while (showMenu != 1) {
+               n++;
+               Console.Write("Please press 1 to show menu: ");
+               showMenu = int.Parse(Console.ReadLine());
+           }
 
-            if(showMenu != 1) {
-                Console.Write("Press 1 to show the menu ");
-            }else {
+           restaurant.displayMenu();
+           Console.WriteLine();
+
+           Console.Write("Press (end) to stop ording or press any key to continue: ");
+           String f = Console.ReadLine();
+
+           while(f != "end"){
                 restaurant.displayMenu();
+                Console.WriteLine(); 
+                Console.Write("Press (end) to stop ording or press any key to continue: ");
                 Console.WriteLine();
+            
             }
+          
+
+           //Console.Write("Pay your order: ");
+           //double money = double.Parse(Console.ReadLine());
+
+           restaurant.getTotal();
+           Console.WriteLine();     
+           
+                
+
+            
+            /*
+              TODO
+
+              #add the price to the total
+              # loop until the person finish buying
+              # Insert the money to pay
+            
+                1.  Method
+                2.  Variable
+                Class
+                3.  Method
+                4.  Variable
+
+                5.  Overloaded method
+
+                6.  String
+                7.  String method
+                8.  Composition
+                9.  Random number
+                10. Array
+                            
+            
+             */
             
 
             
--- Restaurant.cs
+++ Restaurant.cs
@@ -2,52 +2,80 @@ using System;
 class Restaurant{
 
 // Instentive variables 
-    private int price;
-    private int total;
+    private double price;
+    private double cost;
+    private double balance;
+    private static int payment = 0;
 
  public Restaurant() {
       price = 0;
-      total = 0;
+      cost = 0;
+  }
+public void Payment(double initialAmount) {
+    balance = initialAmount;
+  }
+public Restaurant(int p, int t) {
+      price = p;
+      cost = t;
 
   }
-//Methods
     //Display Menu
     public void displayMenu(){
         // display menu with the price
-    Console.Write("(1) Brown Stew Chicken - $5.89 \n"+
-                "(2) Fried Chicken - $7.89 \n"+
-                "(3) Jerk Chicken - $9.89 \n" +
-                "(4) Curry Goat - $8.89 \n" +
-                "(5) Steam Fish - $10.89 \n" 
-     ); 
-     Console.WriteLine();
-     Console.WriteLine("Please make you selection");
+        String menu ="###############################\n"+
+                "----Menu----\n"+
+                "(A) Brown Stew Chicken           $5.89 \n"+
+                "(B) Fried Chicken                $7.89 \n"+
+                "(C) Jerk Chicken                 $9.89 \n" +
+                "(D) Curry Goat                   $8.89 \n" +
+                "(E) Steam Fish                   $10.89 \n"+
+                "###############################\n"
+                ; 
+                
+     Console.WriteLine(menu);
+     Console.WriteLine("Please make you selection: ");
      SelectIterm(Convert.ToChar(Console.ReadLine().ToUpper()));
 }
-        
+
     //Select the food 
     private void SelectIterm(char selection) {
        bool selectionOk = false;
        while(!selectionOk){
            switch(selection){
-                case '1':
+                case 'A':
                     Console.WriteLine("Brown Stew Chicken was added");
+                    price = 5.89;
+                    cost =+ price;
+                    Payment(cost);
+                    Console.WriteLine("${0}", cost);
                     selectionOk = true;
                     break;
-                case '2':
+                case 'B':
                     Console.WriteLine("Fried Chicken was added");
+                    price = 7.89;
+                    cost =+ price;
+                    Console.WriteLine("${0}", cost);
                     selectionOk = true;
                     break; 
-                case '3':
+                case 'C':
                     Console.WriteLine("Jerk Chicken was added");
+                    price = 9.89;
+                    cost =+ price;
+                    Console.WriteLine("${0}", cost);
                     selectionOk = true;
                     break;
-                case '4':
+                case 'D':
                     Console.WriteLine("Curry Goat was added");
+                    price = 8.89;
+                    cost =+ price;
+                    Console.WriteLine("${0}", cost);
                     selectionOk = true;
                     break;
-                case '5':
+                case 'E':
                     Console.WriteLine("Steam Fish was added");
+                    price = 10.89;
+                    cost =+ price;
+                    Console.WriteLine("${0}", cost);
                     selectionOk = true;
                     break;
                 default:
@@ -60,6 +88,11 @@ class Restaurant{
        }
 
     }
+
+   public double getTotal(){
+       payment++;
+       return cost;
+   }
     //Pay
     // Return change
 }
