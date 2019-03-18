1eee7517167f2205c0ce42d75e6885c4b922363d
Onasi Cuevas <onasic96@gmail.com>
2019-03-18 00:57:07 -0400

    finishing displaying the menu

--- Program.cs
+++ Program.cs
@@ -5,14 +5,28 @@ namespace project21
     class Program
     {
         static void Main(string[] args) {
-
+            int showMenu; 
             string[] workers = {"Luis", "Pedro", "Chris", "Perla"};
+
             Random r = new Random();
-
+            Restaurant restaurant = new Restaurant();
             Console.WriteLine("**********************");
-            Console.WriteLine("Welcome to La Familia Restaurante");
-            Console.WriteLine("Hello! My name is  {0}, and going to take your order today", workers[r.Next(0,5)]);
+            Console.WriteLine("Welcome G Press!");
+            Console.WriteLine("Hello! My name is  {0}, and going to take your order today", workers[r.Next(0,4)]);
+            Console.Write("Press 1 to show the menu ");
+            showMenu = int.Parse(Console.ReadLine());
 
+            if(showMenu != 1) {
+                Console.Write("Press 1 to show the menu ");
+            }else {
+                restaurant.displayMenu();
+                Console.WriteLine();
+            }
+            
+
+            
+            
+            
 
  // loop until you finished orden
 
--- Restaurant.cs
+++ Restaurant.cs
@@ -1,17 +1,67 @@
+using System;
 class Restaurant{
 
 // Instentive variables 
-    private int cost_of_food = 0;
+    private int price;
+    private int total;
 
+ public Restaurant() {
+      price = 0;
+      total = 0;
 
-
-
+  }
 //Methods
     //Display Menu
     public void displayMenu(){
+        // display menu with the price
+    Console.Write("(1) Brown Stew Chicken - $5.89 \n"+
+                "(2) Fried Chicken - $7.89 \n"+
+                "(3) Jerk Chicken - $9.89 \n" +
+                "(4) Curry Goat - $8.89 \n" +
+                "(5) Steam Fish - $10.89 \n" 
+     ); 
+     Console.WriteLine();
+     Console.WriteLine("Please make you selection");
+     SelectIterm(Convert.ToChar(Console.ReadLine().ToUpper()));
+}
         
-    }
     //Select the food 
+    private void SelectIterm(char selection) {
+       bool selectionOk = false;
+       while(!selectionOk){
+           switch(selection){
+                case '1':
+                    Console.WriteLine("Brown Stew Chicken was added");
+                    selectionOk = true;
+                    break;
+                case '2':
+                    Console.WriteLine("Fried Chicken was added");
+                    selectionOk = true;
+                    break; 
+                case '3':
+                    Console.WriteLine("Jerk Chicken was added");
+                    selectionOk = true;
+                    break;
+                case '4':
+                    Console.WriteLine("Curry Goat was added");
+                    selectionOk = true;
+                    break;
+                case '5':
+                    Console.WriteLine("Steam Fish was added");
+                    selectionOk = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalied Selection. Please re-enter your selection: ");
+                    selection = Convert.ToChar(Console.ReadLine().ToUpper());
+                    selectionOk = false;
+                    break;
+            }
+           
+       }
+
+    }
     //Pay
     // Return change
 }
+
+
